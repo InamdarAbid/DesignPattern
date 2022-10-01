9bc5db196c4bec9ec15ff36255e8677687de4d93
InamdarAbid <inamdarabid7@gmail.com>
2022-10-01 16:15:29 +0530

    Singleton eg added

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace Singleton
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Singleton Design Pattern";
+            
+            //Below line gives error because class is protected
+            //var test = new SingletonImplementation();
+        
+            var instace1 = SingletonImplementation.Instance;
+            var instace2 = SingletonImplementation.Instance;  
+            
+            if(instace1==instace2 && instace2 == SingletonImplementation.Instance)
+            {
+                Console.WriteLine("Instances are same");
+            }
+
+            instace1.Log("Method called from Instance 1");
+            instace2.Log("Method called from instance 2");
+            SingletonImplementation.Instance.Log("Method called withou object creation");
+        
         }
     }
 }
---
+++ Singleton/SingletonImplementation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singlaton implementation example using logger 
+    /// </summary>
+    public class SingletonImplementation
+    {
+        //Onely one instance of this object is created becuase it's static 
+        private static SingletonImplementation? _loggerInstance;
+
+
+        /// <summary>
+        /// instance access
+        /// </summary>
+        /// Becuase of public static method above instance will be accesible globally and only one instance will be available.
+        public static SingletonImplementation Instance
+        {
+            get 
+            {
+                if(_loggerInstance == null)
+                {
+                    _loggerInstance = new SingletonImplementation();
+                }
+                return _loggerInstance;
+            }
+        }
+
+        protected SingletonImplementation()
+        {
+
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("Message is from {0}",message);
+        }
+
+    }
+}
